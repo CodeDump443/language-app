2e4cc9655e97f9bae044efa0ab140f384d6f79b8
CodeDump443 <raliia.0118z@gmail.com>
2025-05-31 18:03:37 +0500

    Add files via upload

---
+++ pages/TermsOfService.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+
+function TermsOfService() {
+  return (
+    <div>
+      <Header />
+      <h1>Условия использования</h1>
+      <p>
+        Используя приложение "Language App", вы соглашаетесь соблюдать правила добросовестного использования. Запрещается использовать приложение для незаконных целей. Мы оставляем за собой право ограничивать доступ в случае нарушений.
+      </p>
+      <p>Для вопросов пишите на: support@example.com</p>
+    </div>
+  );
+}
+
+export default TermsOfService;
